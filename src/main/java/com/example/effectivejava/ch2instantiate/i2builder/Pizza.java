44e8ca89b0616d3e09c9ab969e2484b78f127d60
lostship <418181589@qq.com>
2022-02-21 10:54:09 +0800

    update comment

--- src/main/java/com/example/effectivejava/ch2instantiate/i2builder/Pizza.java
+++ src/main/java/com/example/effectivejava/ch2instantiate/i2builder/Pizza.java
@@ -1,5 +1,8 @@
 package com.example.effectivejava.ch2instantiate.i2builder;
 
+import static com.example.effectivejava.ch2instantiate.i2builder.Pizza.Topping.*;
+import static com.example.effectivejava.ch2instantiate.i2builder.NyPizza.Size.*;
+
 import java.util.EnumSet;
 import java.util.Set;
 
@@ -24,6 +27,9 @@ public abstract class Pizza {
 
         abstract Pizza build();
 
+        // Subclasses must override this method to return "this"
+        // 它和抽象的self方法一样，允许在子类中适当地进行方法链接，不需要转换类型。
+        // 这个针对Java缺乏self类型的解决方案，被称作模拟的self类型(simulated self-type)。
         protected abstract T self();
     }
 
@@ -33,5 +39,12 @@ public abstract class Pizza {
 
     protected Set<Topping> getToppings() {
         return toppings;
+    }
+
+    public static void main(String[] args) {
+        NyPizza nyPizza = new NyPizza.Builder(SMALL).addTopping(SAUSAGE).addTopping(ONION).build();
+        System.out.println(nyPizza);
+        Calzone calzone = new Calzone.Builder().addTopping(HAM).sauceInside().build();
+        System.out.println(calzone);
     }
 }
